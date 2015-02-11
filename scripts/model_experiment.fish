8334b272c402fe9f6d43e757ba49eb8f22500683
Torben Muldvang Andersen <muldvang@gmail.com>
2015-02-11 11:58:14 +0100

    Scripts for running sequence and model experiments.

---
+++ scripts/model_experiment.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/fish
+
+set code_dir ~/Repositories/thesis-code
+set report_dir ~/Repositories/thesis-report
+for k in 2 4 8 16 32 64 128 256 512 1024
+        for j in (seq 5)
+                $code_dir/release/experiments/model $report_dir/data/1000000.seq $report_dir/data/ $k > $report_dir/plot_data/model.data
+        end
+end
