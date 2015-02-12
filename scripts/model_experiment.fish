1d8f91fdf6ce2421024368337d1e24fa684fe8e9
Torben Muldvang Andersen <muldvang@gmail.com>
2015-02-12 10:03:40 +0100

    More data points for model experiment.

--- scripts/model_experiment.fish
+++ scripts/model_experiment.fish
@@ -3,8 +3,8 @@
 set code_dir ~/Repositories/thesis-code
 set report_dir ~/Repositories/thesis-report
 rm $report_dir/plot_data/model.data
-for k in 2 4 8 16 32 64 128 256 512 1024
+for k in 2 3 4 6 8 12 16 24 32 48 64 96 128 192 256 384 512 768 1024
         for j in (seq 5)
-                eval $code_dir/release/experiments/model $report_dir/data/1000000.seq $report_dir/data/ $k >> $report_dir/plot_data/model.data
+                eval $code_dir/release/experiments/model $report_dir/data/100000.seq $report_dir/data/ $k >> $report_dir/plot_data/model.data
         end
 end
