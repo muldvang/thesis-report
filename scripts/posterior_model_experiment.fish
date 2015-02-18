01d136bb48d347abb9fa1921ff5c3407b58b015e
Torben Muldvang Andersen <muldvang@gmail.com>
2015-02-18 16:20:01 +0100

    Scripts for running posterior experiments.

---
+++ scripts/posterior_model_experiment.fish
@@ -0,0 +1,10 @@
+#!/usr/bin/fish
+
+set code_dir ~/Repositories/thesis-code
+set report_dir ~/Repositories/thesis-report
+rm $report_dir/plot_data/model.data
+for k in 2 3 4 6 8 12 16 24 32 48 64 96 128 192 256 384 512 768 1024
+        for j in (seq 5)
+                eval $code_dir/release/experiments/posterior_model $report_dir/data/100000.seq $report_dir/data/ $k >> $report_dir/plot_data/model.data
+        end
+end
