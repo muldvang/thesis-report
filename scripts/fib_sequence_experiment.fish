6bac3a9e61f93372871239b92be3be81da9f0a48
Torben Muldvang Andersen <muldvang@gmail.com>
2015-02-20 10:37:06 +0100

    Fixed script.

--- scripts/fib_sequence_experiment.fish
+++ scripts/fib_sequence_experiment.fish
@@ -3,6 +3,8 @@
 set code_dir ~/Repositories/thesis-code
 set report_dir ~/Repositories/thesis-report
 rm $report_dir/plot_data/fib_sequence.data
-for i in (seq 32)
-        eval $code_dir/release/experiments/sequence $report_dir/data/16.hmm "$report_dir/data/fib$i.seq" (math (wc -c < "$report_dir/data/fib$i.seq") / 2) >> $report_dir/plot_data/fib_sequence.data
+for i in (seq 2 32)
+        for j in (seq 5)
+                eval $code_dir/release/experiments/sequence $report_dir/data/16.hmm "$report_dir/data/fib$i.seq" (math (wc -c < "$report_dir/data/fib$i.seq") / 2) >> $report_dir/plot_data/fib_sequence.data
+        end
 end
