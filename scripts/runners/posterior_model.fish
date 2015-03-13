1e4040054e7148c7ac01ac3837a871364470cad5
Torben Muldvang Andersen <muldvang@gmail.com>
2015-03-13 13:42:36 +0100

    Don't run posterior experiments multiple times.

--- scripts/runners/posterior_model.fish
+++ scripts/runners/posterior_model.fish
@@ -5,7 +5,7 @@ set report_dir ~/Repositories/thesis-report
 set results_file $report_dir/plot_data/(basename -s .fish (status -f)).data
 rm $results_file
 for N in 2 3 4 6 8 12 16 24 32 48 64 96 128 192 256 384 512
-        for j in (seq -w 20)
+        for j in (seq -w 1)
                 eval $code_dir/release/experiments/bench_posterior $report_dir/data/10000_{$j}.seq 10000 $report_dir/data/$N.hmm $N $results_file
         end
 end
