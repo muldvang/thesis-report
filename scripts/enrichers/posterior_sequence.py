e1901e42ebc43c36f3059ba6db909acf363870c4
Torben Muldvang Andersen <muldvang@gmail.com>
2015-03-13 14:02:16 +0100

    Run posterior experiments two times.

---
+++ scripts/enrichers/posterior_sequence.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+'''Transform a data file by adding more relevant columns.'''
+
+import pandas as pd
+import sys
+import os
+
+if not len(sys.argv) == 2:
+    print("Please provide filename")
+    exit(1)
+
+path = str(sys.argv[1])
+
+df = pd.io.parsers.read_table(path, sep=' ')
+df.columns = ["N",
+              "T",
+              "simple_time",
+              "zipHMMlib_time"]
+
+# Compute mean and std.
+res = pd.DataFrame()
+for _, group in df.groupby(['T']):
+    # Average the measurements.
+    group_frame = pd.DataFrame()
+    for c in df.columns:
+        std = group.loc[:, c].std()
+        mean = group.loc[:, c].mean()
+        group_frame[c] = pd.Series(mean)
+        group_frame[c + '_std'] = pd.Series(std)
+
+    res = res.append(group_frame)
+
+file_name, file_extension = os.path.splitext(path)
+
+res.to_csv(file_name + "_transformed" + file_extension, sep=' ', index=False)
