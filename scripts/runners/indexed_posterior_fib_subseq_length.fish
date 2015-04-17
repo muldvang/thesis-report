7b823379e7af39079afec5478505c5b4ee705d7d
Torben Muldvang Andersen <muldvang@gmail.com>
2015-04-17 13:51:05 +0200

    Fixed bug in script.

--- scripts/runners/indexed_posterior_fib_subseq_length.fish
+++ scripts/runners/indexed_posterior_fib_subseq_length.fish
@@ -6,6 +6,6 @@ set results_file $report_dir/plot_data/(basename -s .fish (status -f)).data
 rm $results_file
 for subseq_length in 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192
         for j in (seq -w 02)
-                eval $code_dir/release/experiments/bench_indexed_posterior $report_dir/data/fib10000_{$j}.seq 10000 $report_dir/data/16.hmm 16 $results_file $subseq_length
+                eval $code_dir/release/experiments/bench_indexed_posterior $report_dir/data/fib19_{$j}.seq 10946 $report_dir/data/16.hmm 16 $results_file $subseq_length
         end
 end
