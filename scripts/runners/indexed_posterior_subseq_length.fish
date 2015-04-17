7adf6bab6ef7e2b9176d0f296bac81ec21157865
Torben Muldvang Andersen <muldvang@gmail.com>
2015-04-17 13:41:51 +0200

    Script for benchmark.

---
+++ scripts/runners/indexed_posterior_subseq_length.fish
@@ -0,0 +1,11 @@
+#!/usr/bin/fish
+
+set code_dir ~/Repositories/thesis-code
+set report_dir ~/Repositories/thesis-report
+set results_file $report_dir/plot_data/(basename -s .fish (status -f)).data
+rm $results_file
+for subseq_length in 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192
+        for j in (seq -w 02)
+                eval $code_dir/release/experiments/bench_indexed_posterior $report_dir/data/10000_{$j}.seq 10000 $report_dir/data/16.hmm 16 $results_file $subseq_length
+        end
+end
