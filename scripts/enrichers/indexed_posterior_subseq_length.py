4e877efba4bb4e4619c52c3f4d7988a401e10d72
Torben Muldvang Andersen <muldvang@gmail.com>
2015-04-23 11:31:06 +0200

    Updated enricher to fit experiment.

--- scripts/enrichers/indexed_posterior_subseq_length.py
+++ scripts/enrichers/indexed_posterior_subseq_length.py
@@ -16,13 +16,21 @@ df = pd.io.parsers.read_table(path, sep=' ', header=None)
 df.columns = ["N",
               "T",
               "subseq_length",
+              "simple_pre_time",
               "simple_running_time",
+              "one_pre_time",
               "one_running_time",
+              "many_pre_time",
               "many_running_time"]
 
+# Total time
+df['simple_total_time'] = df['simple_pre_time'] + df['simple_running_time']
+df['one_total_time'] = df['one_pre_time'] + df['one_running_time']
+df['many_total_time'] = df['many_pre_time'] + 500 * df['many_running_time']
+
 # Running time compared to Simple
-df['one_running_ratio'] = df['simple_running_time'] / df['one_running_time']
-df['many_running_ratio'] = df['simple_running_time'] / df['many_running_time']
+df['one_total_ratio'] = df['simple_total_time'] / df['one_total_time']
+df['many_total_ratio'] = 500 * df['simple_total_time'] / df['many_total_time']
 
 # Compute mean and std.
 res = pd.DataFrame()
