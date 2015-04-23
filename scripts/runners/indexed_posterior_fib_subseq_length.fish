468c0e679e1aaff804d97126341ba53d77f60ac5
Torben Muldvang Andersen <muldvang@gmail.com>
2015-04-23 09:19:26 +0200

    Run more indexed posterior experiments.

--- scripts/runners/indexed_posterior_fib_subseq_length.fish
+++ scripts/runners/indexed_posterior_fib_subseq_length.fish
@@ -5,7 +5,7 @@ set report_dir ~/Repositories/thesis-report
 set results_file $report_dir/plot_data/(basename -s .fish (status -f)).data
 rm $results_file
 for subseq_length in 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192
-        for j in (seq 5)
+        for j in (seq 100)
                 eval $code_dir/release/experiments/bench_indexed_posterior $report_dir/data/fib19.seq 10946 $report_dir/data/16.hmm 16 $results_file $subseq_length
         end
 end
