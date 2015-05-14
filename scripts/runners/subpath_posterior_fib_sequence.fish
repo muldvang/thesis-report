eea270a78bc6fb7f593c05c90bab24b6d422a18e
Torben Muldvang Andersen <muldvang@gmail.com>
2015-05-14 13:14:42 +0200

    Renamed indexed posterior to subpath posterior.

--- scripts/runners/subpath_posterior_fib_sequence.fish
+++ scripts/runners/subpath_posterior_fib_sequence.fish
@@ -7,6 +7,6 @@ rm $results_file
 for i in (seq 15 32)
         set T (math (wc -c < $report_dir/data/fib$i.seq) / 2)
         for j in (seq 5)
-                eval $code_dir/release/experiments/bench_indexed_posterior $report_dir/data/fib{$i}.seq $T $report_dir/data/16.hmm 16 $results_file 200
+                eval $code_dir/release/experiments/bench_subpath_posterior $report_dir/data/fib{$i}.seq $T $report_dir/data/16.hmm 16 $results_file 200
         end
 end
--- scripts/runners/subpath_posterior_subseq_length.fish
+++ scripts/runners/subpath_posterior_subseq_length.fish
@@ -7,7 +7,7 @@ rm $results_file
 for subseq_length in 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192
         for j in (seq -w 20)
                 for k in (seq 100)
-                        eval $code_dir/release/experiments/bench_indexed_posterior $report_dir/data/10000_{$j}.seq 10000 $report_dir/data/16.hmm 16 $results_file $subseq_length
+                        eval $code_dir/release/experiments/bench_subpath_posterior $report_dir/data/10000_{$j}.seq 10000 $report_dir/data/16.hmm 16 $results_file $subseq_length
                 end
         end
 end
