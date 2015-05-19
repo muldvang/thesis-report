770984783f21384635e408544623201a0cfe91b3
Torben Muldvang Andersen <muldvang@gmail.com>
2015-05-19 17:39:30 +0200

    Make runners run 20 iterations for random data.

--- scripts/runners/subpath_posterior_sequence.fish
+++ scripts/runners/subpath_posterior_sequence.fish
@@ -5,7 +5,7 @@ set report_dir ~/Repositories/thesis-report
 set results_file $report_dir/plot_data/(basename -s .fish (status -f)).data
 rm $results_file
 for T in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608
-        for j in (seq -w 02)
+        for j in (seq -w 20)
                 eval $code_dir/release/experiments/bench_subpath_posterior $report_dir/data/{$T}_{$j}.seq $T $report_dir/data/16.hmm 16 $results_file 200
         end
 end
