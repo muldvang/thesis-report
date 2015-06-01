77198b6cffce30cd4f64ad2082c78cde676b9a0e
Torben Muldvang Andersen <muldvang@gmail.com>
2015-06-01 15:40:11 +0200

    Priliminary result is obtained faster.

--- scripts/runners/viterbi_model.fish
+++ scripts/runners/viterbi_model.fish
@@ -4,9 +4,9 @@ set code_dir ~/Repositories/thesis-code
 set report_dir ~/Repositories/thesis-report
 set results_file $report_dir/plot_data/(basename -s .fish (status -f)).data
 rm $results_file
-for N in 2 3 4 6 8 12 16 24 32 48 64 96 128 192 256 384 512 768 1024
-        echo $N
-        for j in (seq -w 20)
+for j in (seq -w 20)
+        echo $j
+        for N in 2 3 4 6 8 12 16 24 32 48 64 96 128 192 256 384 512 768 1024
                 eval $code_dir/release/experiments/bench_viterbi $report_dir/data/10000_{$j}.seq 10000 $report_dir/data/$N.hmm $N $results_file
         end
 end
