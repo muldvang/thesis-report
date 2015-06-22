4c223a06d04c0364c8e654c5c23824cf7a153304
Torben Muldvang Andersen <muldvang@gmail.com>
2015-06-22 12:43:43 +0200

    Viterbi fib model results.

---
+++ scripts/runners/viterbi_fib_model.fish
@@ -0,0 +1,14 @@
+#!/usr/bin/fish
+
+set code_dir ~/Repositories/thesis-code
+set report_dir ~/Repositories/thesis-report
+set results_file $report_dir/plot_data/(basename (status -f) .fish).data
+echo $results_file
+rm $results_file
+eval $code_dir/release/experiments/bench_viterbi $report_dir/data/fib19.seq 10000 $report_dir/data/$N.hmm $N /dev/null
+for j in (seq -w 5)
+        echo $j
+        for N in 2 3 4 6 8 12 16 24 32 48 64# 96 128 192 256 384 512
+                eval $code_dir/release/experiments/bench_viterbi $report_dir/data/fib19.seq 10000 $report_dir/data/$N.hmm $N $results_file
+        end
+end
